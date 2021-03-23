0f9ee06af897a66634999a3487978ac8467e979a
SeungminLee <seungmin.lee@dnx.kr>
2021-03-23 19:50:53 +0900

    ADD: re-first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# twitter_project
+
+<hr>
+
+## 🤖 netflix update bot 🤖  
+
+| Start Date      | 2021-03-16 |
+|-----------------|------------|
+| End Date        |            |
+| Twitter Account | [@Devleelee](https://twitter.com/DevLeelee) |  
+
+Project name: netflix update bot <br> 
+Purpose: tweeting netflix's upcoming movie and drama in twitter for myself. I don't want to go to netflix website and check the info regulary. <br> 
+Dev stack: (fill later) <br> 
+
---
+++ crawl/netflix.py
@@ -0,0 +1,53 @@
+# file for crawl netflix info site
+
+import requests
+from bs4 import BeautifulSoup
+import os
+import dload
+import datetime
+
+
+def createFolder(fullPath):
+    try:
+        if not os.path.exists(fullPath):
+            os.makedirs(fullPath)
+    except OSError:
+        print('ERROR: Creating dir.' + fullPath)
+
+
+def crawl():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    IMG_DIR = f'{BASE_DIR}/img/netflix'
+
+    TARGET_URL = 'https://movie.daum.net/premovie/netflix?flag=Y'
+
+
+    TARGET_SELECTOR = '.item_poster'
+    DATE_FORMAT = '%Y%m%d'
+
+    _req = requests.get(TARGET_URL)
+    _req.encoding = None
+    html = _req.content
+    _soup = BeautifulSoup(html, 'html.parser')
+
+    # 정보 크롤링
+    infos = _soup.select(TARGET_SELECTOR)
+
+    today = datetime.datetime.now()
+    fToday = today.strftime(DATE_FORMAT)
+
+    container = {}
+
+    for info in infos:
+        title = info.select_one('.thumb_cont > .tit_item > a').text
+        date = info.select_one('.thumb_cont > .txt_info > .txt_num').text
+        container[title] = date
+
+        _check_img = info.select_one('.thumb_item > .poster_movie > img')
+
+        if _check_img is None:
+            del container[title]
+            continue
+
+        img = info.select_one('.thumb_item > .poster_movie > img')['src']
+        dload.save(img, f'{IMG_DIR}\\{title}_{fToday}.png')
